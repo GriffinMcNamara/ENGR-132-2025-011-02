d8eecb6af78ae306a0a1317a0eedc6bc8ae6eb61
GriffinMcNamara <FerrariRaceGuy@gmail.com>
2025-04-01 10:02:13 -0400

    Add files via upload

---
+++ M1A_main_011_02_McNama36.m
@@ -0,0 +1,58 @@
+function M1A_main_011_02_McNama36
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132 
+% Program Description 
+% Function to process data files using subfunctions for data management.
+% The processed data will then be displayed in professionally formatted 
+% figures and plots.
+%
+% Function Call
+% the function call is: M1A_main_011_02_McNama36
+%
+% Input Arguments
+%
+% Output Arguments
+%
+% Assignment Information
+%   Assignment:     M1, Problem 4
+%   Team member:   Griffin McNamara, McNama36@purdue.edu
+%   Team ID:        011-02
+%   Academic Integrit
+% y:
+%     [] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%     Peers we worked with: John Catalan, catalan0@purdue.edu
+%                           Shrey Panicker, panickes@purdue.edu
+%                           Aidan Policelli, apolicel@purdue.edu
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+data = 0;
+
+%% ____________________
+%% CALCULATIONS
+
+% move the data from one subfunction to the other
+[data_to_3] = M1A_sub2_001_02_panickes(data);
+[data_out] = M1A_sub3_011_02_catalan0 (data_to_3);
+
+
+
+%% ____________________
+%% FORMATTED TEXT/FIGURE DISPLAYS
+
+
+%% ____________________
+%% RESULTS
+
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
+
+
+
---
+++ M1A_sub2_001_02_panickes.m
@@ -0,0 +1,51 @@
+function [data_out] = M1A_sub2_001_02_panickes(data_in)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132 
+% Program Description 
+%
+% This subfunction can take a data array input and will pass it back out,
+% and print out the line of code required by the assignment
+%
+% Function Call
+%
+% M1A_sub2_001_02_panickes()
+%
+% Input Arguments
+%
+% data_in - inputted data
+%
+% Output Arguments
+%
+% data_out - outputted data
+%
+% Assignment Information
+%   Assignment:     M01, Problem #4
+%   Team member:    Shrey Panicker, panickes@purdue.edu [repeat for each person]
+%   Team ID:        011-02
+%   Academic Integrity:
+%     [] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%     Peers we worked with: Name, login@purdue [repeat for each]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+
+    data_out = data_in;
+    
+%% ____________________
+
+%% FORMATTED TEXT/FIGURE DISPLAYS
+    
+   fprintf("Data successfully passed to subfunction 2 programmed by" + ...
+   " Shrey Panicker \n");
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
+
+
+
