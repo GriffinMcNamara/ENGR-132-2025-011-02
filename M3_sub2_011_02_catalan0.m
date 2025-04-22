6fb49e540dece38505ad371fc58a6f5d4f229dc9
GriffinMcNamara <FerrariRaceGuy@gmail.com>
2025-04-22 10:43:08 -0400

    Went back to the original cleaning algorithum because all me changes did not help
    Messed with main to change the number of loops to clean the data

--- M3_sub2_011_02_catalan0.m
+++ M3_sub2_011_02_catalan0.m
@@ -26,72 +26,31 @@ function [avg_speed_data] = M3_sub2_011_02_catalan0(speed_data)
 
 %% ____________________
 %% INITIALIZATION
-[rows_size, column_size] = size(speed_data); 
-avg_speed_data = nan(rows_size, column_size); 
-
+[rows_size, column_size] = size(speed_data); %Assigns values to each 
+                                             %variable which is the size of
+                                             %the row or column.
+avg_speed_data = nan(rows_size - 1, column_size); % We're only getting
+                                                  % "size - 1" averages
 %% ____________________
 %% CALCULATIONS
-
-% Moving average with window = 3, ignoring NaNs
-window_size = 3;
-half_window = floor(window_size / 2);
-
-for col = 1:column_size
-    for row = 1:rows_size
-        % Define the window boundaries
-        lower = max(1, row - half_window);
-        upper = min(rows_size, row + half_window);
-        
-        window_vals = speed_data(lower:upper, col);
-        window_vals = window_vals(~isnan(window_vals)); % exclude NaNs
-        
-        if ~isempty(window_vals)
-            avg_speed_data(row, col) = mean(window_vals); 
+for ii = 1:column_size
+    for jj = 1:rows_size - 1 % Stops at second-to-last point so it stays in
+                             % bounds, same reason as stated earlier
+        if ~isnan(speed_data(ii,jj)) && ~isnan(speed_data(ii+1, jj))
+            avg_speed_data(ii,jj) = ((speed_data(ii,jj) + speed_data(ii+1,jj)) / 2); 
+            % Average of the two becomes new data point. We also use 
+            % ~isnan because this actually returns a 1 or 0 that we can use
+            % in our conditional statement
+ 
         else
-            avg_speed_data(row, col) = NaN;
-        end
-    end
-end
+            avg_speed_data(ii,jj) = (nan); 
+            % If the value we are observing is nan then it just stays nan
 
-% Additional optional smoothing (repeat pass)
-repeat_passes = 1;
+        end 
+    end 
+end 
 
-for pass = 1:repeat_passes
-    for col = 1:column_size
-        col_data = avg_speed_data(:, col);
-        valid_idx = ~isnan(col_data);
-        valid_data = col_data(valid_idx);
 
-        smoothed = movmean(valid_data, 5, 'Endpoints', 'shrink');
-
-        new_col = avg_speed_data(:, col); % Start with current data
-        new_col(valid_idx) = smoothed;
-        avg_speed_data(:, col) = new_col;
-    end
-end
-
-% Final median filter to reduce spikes
-median_window = 3;
-half_window = floor(median_window / 2);
-
-for col = 1:column_size
-    col_data = avg_speed_data(:, col);
-    new_col = nan(rows_size, 1);
-
-    for i = 1:rows_size
-        start_idx = max(1, i - half_window);
-        end_idx = min(rows_size, i + half_window);
-
-        window_data = col_data(start_idx:end_idx);
-        window_data = window_data(~isnan(window_data));
-
-        if ~isempty(window_data)
-            new_col(i) = median(window_data);
-        end
-    end
-
-    avg_speed_data(:, col) = new_col;
-end
 
 %% ____________________
 %% ACADEMIC INTEGRITY STATEMENT
