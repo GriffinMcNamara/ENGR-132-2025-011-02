2a2a4886700a8504d620b5942fd4725cbaf59fda
GriffinMcNamara <FerrariRaceGuy@gmail.com>
2025-04-23 21:25:43 -0400

    Finished 4.a

--- M3_benchmark_011_02_McNama36.m
+++ M3_benchmark_011_02_McNama36.m
@@ -27,10 +27,16 @@ function M3_benchmark_011_02_McNama36(real_data, bench_data, data_time, ...
 %% ____________________
 %% INITIALIZATION
 SSE_total = 0;
-single_car_data_ave = zeros(size(bench_data, 1), 1);
-i_single_car = 1;
 
+% Estimate yL and yH from velocity data
+yL_guess = [-0.09, -0.22, 0.19];
+yH_guess = [25.08, 24.72, 24.18];
 
+% Rough guess of when the velocity starts increasing
+ts_guess = [6.21, 9.39, 6.85];
+
+% Initial guess for time constant
+tau_guess = [1.51, 1.96, 2.80];
 
 %% ____________________
 %% FORMATTED TEXT/FIGURE DISPLAYS
@@ -39,6 +45,7 @@ i_single_car = 1;
 for car = 1:Num_cars
 
     figure(car + 3)
+    hold on
     set(figure(car + 3), 'Name', ['Vehicle Speed vs. Time: Benchmark ' ...
         'Data and First-Order Model Comparison'], 'NumberTitle', 'off');
     % Get screen size (in pixels)
@@ -46,46 +53,48 @@ for car = 1:Num_cars
     % Set figure position to cover the whole screen
     set(gcf, 'Position', screen_size);
 
-    for tyre = 1:Num_tyres
-        subplot(2, 2, tyre) % Move to the next subplot tile
-        hold on  % Allow multiple lines on the same subplot
+    % for tyre = 1:Num_tyres
+    % 
+    %         % fill the matrix with values for use at the end of the outer
+    %         % function
+    %         single_car_data_ave(:, 1) = real_data(:, ...
+    %             car + 1) + single_car_data_ave(:, 1);
+    % end   
+    model = M3_benchmark_sub_1_011_02_McNama36(bench_data, ...
+         data_time, yL_guess(car), yH_guess(car), ts_guess(car) ...
+         , tau_guess(car));
+    % single_car_data_ave = mean(bench_data, 2);
+    %plot the average of the test data
+    plot(bench_data(:, 1), bench_data(:, car + 1), 'LineWidth', 1.5);
+    %plot our model
+    plot(data_time, model, 'LineWidth', 1.5)
 
+    % plot formatting
+    title(sprintf('Car %d', car))
+    xlabel('Time Index')
+    ylabel('Speed (or Output)')
+    legend("average of the test data", "model",'Location', 'best')
+    grid on
 
+    %% SSE CALCULATIONS
 
-            % fill the matrix with values for use at the end of the outer
-            % function
-            single_car_data_ave(:, 1) = bench_data(:, ...
-                car + 1) + single_car_data_ave(:, 1);
-         
-        model = M3_benchmark_sub_1_011_02_McNama36(single_car_data_ave, ...
-             data_time);
-        % single_car_data_ave = mean(bench_data, 2);
-        %plot the average of the test data
-        plot(bench_data(:, 1), bench_data(:, car + 1), 'LineWidth', 1.5);
-        %plot our model
-        plot(data_time, model, 'LineWidth', 1.5)
-
-        %% SSE CALCULATIONS
-        for control = 1 : (length(bench_data) - 1)
-            SSE_total = (model(control) - bench_data(control + 1))...
-                .^ 2 + SSE_total;
-        end
-        
-        %the error devided by the number of datapoints I have
-        ave_error_per_point = SSE_total ./ size(bench_data, 1);
-
-        % Subplot formatting
-        title(sprintf('Car %d - Tyre %d', car, tyre))
-        xlabel('Time Index')
-        ylabel('Speed (or Output)')
-        legend("average of the test data", "model",'Location', 'best')
-        grid on
-        hold off
-    end
+        SSE_total = sum((model - bench_data(2:end, car + 1))...
+            .^ 2);
+    
+    %the error devided by the number of datapoints I have
+    ave_error_per_point = SSE_total ./ size(bench_data, 1);
+    
+    annotation('textbox', [0.35, 0.08, 1, 0.1], ...
+    'String', sprintf('Average SSE per point: %.4f', ave_error_per_point), ...
+    'EdgeColor', 'none', ...
+    'HorizontalAlignment', 'center', ...
+    'FontSize', 12, ...
+    'FontWeight', 'bold');
 
     % set the index for the tempuary matrix to 1 for the next loop
-    i_single_car = 1;
-    single_car_data = [];
+    single_car_data_ave = zeros(size(real_data, 1), 1);
+    SSE_total = 0;
+    hold off
 end
 
 
--- M3_benchmark_sub_1_011_02_McNama36.m
+++ M3_benchmark_sub_1_011_02_McNama36.m
@@ -1,4 +1,5 @@
-function y = M3_benchmark_sub_1_011_02_McNama36(real_data, data_time)
+function y = M3_benchmark_sub_1_011_02_McNama36(real_data, data_time, ...
+    yL, yH, ts, tau)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ENGR 132 - Exponential Model Function
 %
@@ -29,23 +30,6 @@ function y = M3_benchmark_sub_1_011_02_McNama36(real_data, data_time)
 n = numel(data_time);
 
 %% INITIAL GUESS SETUP
-
-% Estimate yL and yH from velocity data
-yL_guess = min(real_data);
-yH_guess = max(real_data);
-
-% Rough guess of when the velocity starts increasing
-[~, idx_start] = max(diff(real_data));
-ts_guess = data_time(idx_start);
-
-% Initial guess for time constant
-tau_guess = 1;
-
-% Extract parameters for clarity
-yL = yL_guess;
-yH = yH_guess;
-ts = ts_guess;
-tau = tau_guess;
 
 % Preallocate output
 y = zeros(size(data_time));
